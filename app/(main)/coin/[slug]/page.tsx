07bb8b1bae932ff615101ba8160eb06d9fc86bf1
wdcs-dhruvdave <dhruv.dave@codezeros.com>
2025-07-16 15:02:01 +0530

    bug Fixes for vercel 2

--- app/(main)/coin/[slug]/page.tsx
+++ app/(main)/coin/[slug]/page.tsx
@@ -1,9 +1,17 @@
-type Props = {
+
+type PageProps = {
   params: {
     slug: string;
   };
 };
 
-export default function CoinPage({ params }: Props) {
-  return <div>Coin slug: {params.slug}</div>;
+export default async function CoinPage({ params }: PageProps) {
+  const { slug } = params;
+
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Coin: {slug}</h1>
+    </div>
+  );
 }
